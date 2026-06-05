d3e583ba676c49ec6e6581eadda4d5fc1882f1f2
vex <github@myxoz.de>
2026-06-06 00:07:08 +0200

    Removed all bin files from git

--- src/de/uni_hamburg/informatik/swt/se2/mediathek/ui/vormerken/VormerkWerkzeug.java
+++ src/de/uni_hamburg/informatik/swt/se2/mediathek/ui/vormerken/VormerkWerkzeug.java
@@ -27,7 +27,6 @@ import de.uni_hamburg.informatik.swt.se2.mediathek.ui.subwerkzeuge.vormerkmedien
  */
 public class VormerkWerkzeug
 {
-	// Test vom Mac und das hier vom meinem HauptPC
     /**
      * Die UI-Komponente der Ausleihe.
      */
